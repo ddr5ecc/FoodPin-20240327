31123736dea879c2ebb59b16e16545d16a5e426e
SL578 <linstephen578@gmail.com>
2024-02-17 16:26:02 -0800

    added placeholder text for restaurantType and restaurantLocations

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -14,24 +14,13 @@ struct RestaurantListView: View {
     
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's chocolate", "Palomino Espresso"]
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino"]
+    var restaurantTypes = ["placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder"]
+    var restaurantLocations = ["placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder", "placeholder"]
     
     var body: some View {
         List {
             ForEach(restaurantNames.indices, id: \.self) { index in
-                HStack(alignment: .top, spacing: 20) {
-                    Image(restaurantImages[index])
-                        .resizable()
-                        .cornerRadius(20)
-                    .frame(width: 120, height: 120)
-                    VStack(alignment: .leading) {
-                        Text(restaurantNames[index])
-                        Text("Type:")
-                            .font(.system(.body, design: .rounded))
-                        Text("Location:")
-                            .font(.system(.subheadline, design: .rounded))
-                            .foregroundColor(.gray)
-                    }
-                }
+                BasicTextImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index])
             }
             .listRowSeparator(.hidden)
         }
@@ -58,4 +47,32 @@ struct RestaurantListView: View {
     RestaurantListView()
         .preferredColorScheme(.dark)
         .modelContainer(for: Item.self, inMemory: true)
+}
+
+struct BasicTextImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .frame(height: 200)
+                .cornerRadius(20)
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+    }
 }
