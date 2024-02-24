542c52514956cc6be2cb84109b6e9bbb826b439b
SL578 <linstephen578@gmail.com>
2024-02-24 14:22:08 -0800

    Added full image view

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -27,29 +27,33 @@ struct RestaurantListView: View {
         }
         .listStyle(.plain)
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
 }
 
+struct RestaurantListView_Previews: PreviewProvider{
+    static var previews: some View{
+        RestaurantListView()
+        
+        RestaurantListView()
+            .preferredColorScheme(.dark)
+        
+        BasicTextImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+            .previewLayout(.sizeThatFits)
+        FullImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+            .previewLayout(.sizeThatFits)
+    }
+}
+/*
 #Preview {
     RestaurantListView()
         .preferredColorScheme(.dark)
         .modelContainer(for: Item.self, inMemory: true)
+    
+    BasicTextImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+        .previewLayout(.sizeThatFits)
+    FullImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+        .previewLayout(.sizeThatFits)
 }
-
+*/
 struct BasicTextImageRow: View {
     
     var imageName: String
@@ -64,6 +68,66 @@ struct BasicTextImageRow: View {
         HStack(alignment: .top, spacing: 20) {
             Image(imageName)
                 .resizable()
+                .frame(height: 200)
+                .cornerRadius(20)
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+                if isFavorite {
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.yellow)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+        }
+        .onTapGesture{
+            showOptions.toggle()
+        }
+        .actionSheet(isPresented: $showOptions){
+            ActionSheet(title: Text("What do you want to do?"),
+                        message: nil,
+                        buttons: [
+                            .default(Text("Reserve a table")){
+                                self.showError.toggle()
+                            },
+                            .default(Text("Mark as favorite")){
+                                self.isFavorite.toggle()
+                            },
+                            .cancel()
+                            ])
+        
+        }
+        .alert(isPresented: $showError){
+            Alert(title: Text("Not yet available"),
+                  message: Text("Sorry, this feature is not available yet. Please retry later."),
+                  primaryButton: .default(Text("OK")),
+                  secondaryButton: .cancel())
+        }
+    }
+}
+
+struct FullImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    @State private var showOptions = false
+    @State private var showError = false
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
                 .frame(height: 200)
                 .cornerRadius(20)
             VStack(alignment: .leading) {
