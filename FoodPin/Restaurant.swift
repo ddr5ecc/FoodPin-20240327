7a0680b79a1452b34b674a55d5c342ac00847626
ddr5ecc <78187435+ddr5ecc@users.noreply.github.com>
2024-03-27 14:08:33 -0700

    Comments

--- FoodPin/Restaurant.swift
+++ FoodPin/Restaurant.swift
@@ -7,13 +7,16 @@
 
 import Foundation
 
+// 建立Restaurant 物件
+
 struct Restaurant {
     var name: String
     var type: String
     var location: String
     var image: String
     var isFavorite: Bool
-    
+
+    // 若有完整參數，將參數宣告為物件
     init(name: String, type: String, location: String, image: String, isFavorite: Bool){
         self.name = name
         self.type = type
@@ -21,7 +24,8 @@ struct Restaurant {
         self.image = image
         self.isFavorite = isFavorite
     }
-    
+
+    // 若無完整參數，指定以下預設參數
     init(){
         self.init(name: "", type: "", location: "", image: "", isFavorite: false)
     }
